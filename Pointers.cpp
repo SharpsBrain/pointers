2a5aa1fa92edb3a89dbb53a5e101c2735ba87bd6
SharpsBrain <109470818+SharpsBrain@users.noreply.github.com>
2022-08-05 22:16:19 +0500

    Add files via upload

---
+++ Pointers.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    //declaration of variables:
+    int x=27;
+
+    //declaration of pointers syntax :
+    /*int *ip;
+    ip =&x;
+    cout<<"Value of x is :";
+    cout<<x<<endl;
+    cout<<"value of ip is:";
+    cout<<ip<<endl;
+    cout<<"value of *ip is:";
+    cout<<*ip<<endl;
+ */
+ /*int *ip;
+ int arr[]={10, 34, 13, 76, 5, 46};
+ ip=arr;
+ for(int x=0;x<6;x++){
+    cout<<*ip<<endl;
+    ip++;
+
+ }
+   */
+   int *ip=NULL;
+   cout<<"Value of ip is:"<<ip;
+    return 0;
+}
+//-----------------Sharpsbrain--------------//
